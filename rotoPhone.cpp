7b5b6d3191b4eddaa50981d0e3f3c3998cd68ae2
Tournevis <arthur.ro92@gmail.com>
2015-10-06 21:18:39 +0200

    :fire: Init :fire:

--- rotoPhone.cpp
+++ rotoPhone.cpp
@@ -1,7 +1,7 @@
 #include "Arduino.h"
 #include "rotoPhone.h"
 
-rotoPhone::rotoPhone(int pin1, int pin2){
+rotoPhone::rotoPhone(uint8_t  pin1, uint8_t  pin2){
   pinMode(pin1, OUTPUT);
   pinMode(pin2, OUTPUT);
   _pin1 = pin1;
@@ -12,14 +12,13 @@ rotoPhone::number(){
   _state1  = digitalRead(_pin1);
   _state2 = digitalRead(_pin2);
 
+  if (isNum && num == 0 && lastNum ==1) {
+    number ++ ;
+  }
   if (_state1 == _state2 ) {
     _isNum = true;
   }else{
     _isNum = false;
     return number;
   }
-  if (isNum && num == 0 && lastNum ==1) {
-    number ++ ;
-  }
-
 }
