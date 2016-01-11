b60f88c8b878d167cbac2e77123fb4efb2eecfe8
tournevis <arthur.ro92@gmail.com>
2016-01-11 11:08:42 +0100

    Renaming :fire:

---
+++ RotoPhone.cpp
@@ -0,0 +1,66 @@
+#include "Arduino.h"
+#include "RotoPhone.h"
+
+RotoPhone::RotoPhone(uint8_t pin1, uint8_t  pin2, bool pullUp){
+  //Turn on pull up resistor in the arduino to avoid short circuit
+  _pullUp = pullUp;
+  _startTime = 0;
+  if(pullUp){
+    pinMode(pin1, INPUT_PULLUP);
+    pinMode(pin2,  INPUT_PULLUP);
+  }else{
+    pinMode(pin1, INPUT);
+    pinMode(pin2,  INPUT);
+  }
+  _pin1 = pin1;
+  _pin2 = pin2;
+}
+
+int8_t RotoPhone::number(){
+  _state1  = digitalRead(_pin1);
+  _state2 = digitalRead(_pin2);
+  if(_pullUp){
+    if (_isNum && _state1 == HIGH && _lastState ==LOW ) {
+      _number ++ ;
+    }
+  }else{
+    if (_isNum && _state1 == LOW && _lastState == HIGH) {
+      _number ++ ;
+    }
+  }
+  if (_state1 == _state2 ) {
+    _isNum = true;
+    if(_startTime == 0 ){
+      _startTime = millis();
+    }
+  }else{
+    if(_finalNumber != _number ){
+      _finalTime = millis() - _startTime;
+    }
+    _finalNumber = _number ;
+    _isNum = false;
+    _startTime = 0;
+    //return _finalNumber =-1 ;
+  //  if(_pullUp){ _finalNumber = _number -1 ; }else{ _finalNumber = _number ;}
+    //return number;
+    _number = -1;
+  }
+  _lastState =digitalRead(_pin2);
+  delay(10);
+  return _finalNumber;
+
+}
+
+unsigned long RotoPhone::numTime(){
+  return _finalTime;
+}
+void RotoPhone::debug(){
+  //if(Serial.available() > 0){
+    Serial.print("Pin state are : Pin1 : ");
+    Serial.print(_state1);
+    Serial.print("    Pin state are Pin2 : ");
+    Serial.print(_state2);
+    Serial.print("    And current number is : ");
+    Serial.println( _number);
+//  }
+}
---
+++ RotoPhone.h
@@ -0,0 +1,24 @@
+#ifndef RotoPhone_h
+#define RotoPhone_h
+#endif
+
+#include "Arduino.h"
+class RotoPhone{
+  public :
+    RotoPhone(uint8_t pin1, uint8_t pin2, bool pullUp = true);
+    int8_t number();
+    unsigned long numTime();
+    void debug();
+  private :
+    uint8_t _number;
+    int8_t _finalNumber;
+    uint8_t _pin1;
+    uint8_t _pin2;
+    uint8_t _lastState;
+    unsigned long _startTime  ;
+    unsigned long _finalTime;
+    bool _state1;
+    bool _state2;
+    bool _isNum;
+    bool _pullUp;
+};
